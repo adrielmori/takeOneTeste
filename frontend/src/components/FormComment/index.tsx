48035da29709d2e2f777915c6500b8b698e2a3a7
MORIAdriel <adrielmori@outlook.com>
2022-03-30 17:03:17 -0300

    Arrumando os create tables

---
+++ frontend/src/components/FormComment/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "components/Auth/";
+import { Link } from "react-router-dom";
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import { ReactComponent as Logo } from "assets/img/LogoLogin.svg";
+import "./styles.css";
+import axios from "axios";
+
+function Comments() {
+}
+
+export default Comments;
